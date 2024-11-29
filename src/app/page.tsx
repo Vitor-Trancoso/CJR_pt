0dbb2749dced690a0193b6086cc321944cd64a6f
Vitor Trancoso <studyvitorm@gmail.com>
2024-11-29 19:59:05 -0300

    append de animações no background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
         <Form />
       </div>
       {/* Seção da Imagem e Texto */}
-      <div className="hidden relative lg:flex h-full w-1/2 flex-col items-center justify-center bg-gradient-to-br from-[#004aad] to-[#cb6ce6]">
-        {/* Imagem */}
+      <div className="hidden relative lg:flex h-full w-1/2 flex-col items-center justify-center bg-gradient-to-br from-[#004aad] to-[#cb6ce6] bg-gradient-to-br animate-gradient">
         <img
           src="/images/unblogo.jpg"
-          alt="Logo da Universidade"
-          className="w-1/4 h-auto mb-4" /* Ajuste no tamanho da imagem */
+          alt="Logo da UnB"
+          className="w-1/3 mx-auto mb-6"
         />
-        {/* Texto Abaixo da Imagem */}
-        <p className="text-white text-lg font-bold">Avaliação de Professores</p>
+        <p className="text-xl text-white font-medium">
+          Avaliação de Professores
+        </p>
       </div>
     </div>
   );
