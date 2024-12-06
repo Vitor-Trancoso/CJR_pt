3475fefefbb5c9305e086714bb6c575cc46a3553
maaduh <duds2992@gmail.com>
2024-12-05 23:08:52 -0300

    correção dados do cadastro, integração front-back na criação do cadastro e direcionamento cadastro > login quando usuário for criado

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,4 +1,57 @@
+'use client'
+import Router, { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [user, setUser] = useState<any>([]);
+  const Router = useRouter();
+  const [users, setUsers] = useState<any>([]);
+
+
+  useEffect(() => {
+      console.log('Initial comments state: ', users);
+      obterUsers()
+  }, [])
+
+  async function obterUsers() {
+    try {
+        const resp = await fetch('http://localhost:3001/user');
+        const users = await resp.json();
+        console.log(users); 
+        setUsers(users);
+    } catch (error) {
+        console.error("Error fetching users: ", error);
+        setUsers([]); 
+    }
+  }
+
+  async function criarUser(){
+    try {
+      const resp = await fetch('http://localhost:3001/user', {
+          method: 'POST', 
+          headers: {
+              'Content-Type' : 'application/json'
+          },
+          body: JSON.stringify(user)
+      })
+    
+      if(resp.ok) {
+        setUser({})
+        const users = await resp.json()
+        setUsers(users)
+        Router.push('/')
+        alert ("usuário criado")
+      }
+      else{
+        console.error('Erro ao criar usuário');
+      }
+    }
+    catch (error){
+      console.error('Erro ao criar usuário', error)
+    } 
+
+  }
+
   return (
     <div className="flex items-center justify-center w-full h-screen bg-gradient-to-br animate-gradient">
       {/* Seção do Formulário */}
@@ -15,27 +68,56 @@ export default function Home() {
         <form className="space-y-6">
           <div>
             <input
+              id ="name"
               type="text"
+              value ={user.name ?? ''}
+              onChange={(e) => setUser({...user, name: e.target.value})}
               placeholder="Nome Completo"
               className="w-full border border-gray-600 rounded-xl px-4 py-3 text-white placeholder-gray-400 bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           <div>
             <input
+              id = "email"
               type="email"
+              value ={user.email ?? ''}
+              onChange={(e) => setUser({...user, email: e.target.value})}
               placeholder="Email"
               className="w-full border border-gray-600 rounded-xl px-4 py-3 text-white placeholder-gray-400 bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           <div>
             <input
+              id = "senha"
               type="password"
               placeholder="Senha"
+              value ={user.senha ?? ''}
+              onChange={(e) => setUser({...user, senha: e.target.value})}
+              className="w-full border border-gray-600 rounded-xl px-4 py-3 text-white placeholder-gray-400 bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div>
+            <input
+              id = "curso"
+              type="text"
+              value = {user.curso ?? ''}
+              placeholder="Curso"
+              onChange={(e) => setUser({...user, curso: e.target.value})}
+              className="w-full border border-gray-600 rounded-xl px-4 py-3 text-white placeholder-gray-400 bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div>
+            <input
+              id = "departamento"
+              type="text"
+              placeholder="Departamento"
+              value = {user.departamento ?? ''}
+              onChange={(e) => setUser({...user, departamento: e.target.value})}
               className="w-full border border-gray-600 rounded-xl px-4 py-3 text-white placeholder-gray-400 bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           <div className="flex justify-center">
-            <button className="w-full py-3 bg-blue-500 text-white rounded-xl font-bold hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 transition">
+            <button onClick={criarUser} className="w-full py-3 bg-blue-500 text-white rounded-xl font-bold hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 transition">
               Criar Conta
             </button>
           </div>
---
+++ src/app/teste/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+import { useEffect, useState } from "react"
+
+export default function Home(){
+
+    const [comment, setComment] = useState<any>([]);
+    const [comments, setComments] = useState<any>([]);
+
+    useEffect(() => {
+        console.log('Initial comments state: ', comments);
+        obterComments()
+    }, [])
+
+    async function obterComments() {
+        try {
+            const resp = await fetch('http://localhost:3001/comments');
+            const comments = await resp.json();
+            console.log(comments); 
+            setComments(comments);
+        } catch (error) {
+            console.error("Error fetching comments: ", error);
+            setComments([]); 
+        }
+    }
+    
+    async function criarComment(){
+        const resp = await fetch('http://localhost:3001/comments', {
+            method: 'POST', 
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(comment)
+        })
+        setComment({})
+        const comments = await resp.json()
+        setComments(comments)
+    }
+
+
+    function renderizarFormComments(){
+        return(
+            <>
+                <div>
+                    <label htmlFor="userID"> userID </label>
+                    <input 
+                        id="userID" 
+                        type="number" 
+                        value={comment.userID ?? ''} 
+                        onChange={(e) => setComment({ ...comment, userID: e.target.value })} 
+                    />
+                </div>
+                <div>
+                    <label htmlFor="avalID"> avalID </label>
+                    <input 
+                        id="avalID" 
+                        type="number" 
+                        value={comment.avalID ?? ''} 
+                        onChange={(e) => setComment({ ...comment, avalID: +e.target.value })} 
+                    />
+                </div>
+                <div>
+                    <label htmlFor="conteudo"> conteudo </label>
+                    <input 
+                        id="conteudo" 
+                        type="text" 
+                        value={comment.conteudo ?? ''} 
+                        onChange={(e) => setComment({ ...comment, conteudo: e.target.value })} 
+                    />
+                </div>
+                <div>
+                    <button onClick={criarComment}> Salvar </button>
+                </div>
+            </>
+        )
+    }
+
+
+
+    return(
+        <div>
+            {renderizarFormComments()}
+        </div>
+    )
+}
